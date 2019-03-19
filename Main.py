9e8edd425c893e2b01f129c2fbbc19281b13f73f
GainAw <wescrank2000@gmail.com>
2019-03-18 20:40:17 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,14 +6,16 @@ def new_password():
     username = input("What is the username: ")
     password = input("What is the pasword: ")
     if password == "random":
-        N = int(input("How long do you want? "))
-        if N < 8:
-            print("Please try again: ")
-            
-        else:
-            random_password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(N))
-            print(f"This is your random password: {random_password}")
-            password = random_password
+        while True:
+            N = int(input("How long do you want? "))
+            if N < 8:
+                print("Please try again: ")
+                continue
+            else:
+                random_password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(N))
+                print(f"This is your random password: {random_password}")
+                password = random_password
+                break
     else:
         pass
     
@@ -21,11 +23,17 @@ def new_password():
 
     password_list.append(website_entry)
     print(password_list)
-
+def find_password():
+    what_website_password = input("What website's password are you trying to find? ")
+    for entry in password_list:
+        if entry["website"] == what_website_password:
+            print(entry["username"])
+            print(entry["password"])
+            break
+    print("Website does not exist!!!")
 while True:
     print("""Hello welcome back
           just to make sure you remember we're dropping the commands as a refresher
-          help: pulls this up again
           show: shows your password for a specific website
           add: Allows you to add extra website info
           """)
@@ -33,13 +41,6 @@ while True:
     if option == "add":
         new_password()
     elif option == "show":
-        pass
-    elif option == "help":
-        print("""Hello welcome back /n
-          just to make sure you remember we're dropping the commands as a refresher/n
-          help: pulls this up again/n
-          show: shows your password for a specific website/n
-          add: Allows you to add extra website info/n
-          """)
+        find_password()
     else:
         print("Please try again correctly: ")
